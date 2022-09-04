ffbce3f6fcc7b38f52634f7d6d46ace31a4e2c54
nishithmunda <nishithmunda9212@gmail.com>
2022-09-05 00:31:15 +0530

    fix: HTMLPlugin import webpack

--- container/src/App.js
+++ container/src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
+  console.log("MarketingApp",MarketingApp)
   return (
     <div>
-      <h1>Hi there!</h1>
+      <h1>Market Place</h1>
       <hr />
       <MarketingApp />
     </div>
